f2b756ee7765440d8cd38f254f41492251710a7f
SrinivasKoppera <jaanusaragandla@gmail.com>
2025-09-30 22:53:27 +0530

    Add logo asset and integrate into Header component; create type declarations for image modules

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,29 +1,27 @@
-import React from 'react';
-import HeroSection from '../components/HeroSection';
-import SplitHeroSection from '../components/SplitHeroSection';
-import EventCarousel from '../components/EventCarousel';
-import FeaturedEventsCarousel from '../components/FeaturedEventsCarousel';
-import EventJourneyCarousel from '../components/EventJourneyCarousel';
-import FeatureCards from '../components/FeatureCards';
-import StatsSection from '../components/StatsSection';
-import ProcessSection from '../components/ProcessSection';
-import TestimonialsSection from '../components/TestimonialsSection';
-import FAQSection from '../components/FAQSection';
-import CTASection from '../components/CTASection';
-import VideoBackgroundSection from '../components/VideoBackgroundSection';
-import GallerySection from '../components/GallerySection';
-import TeamSection from '../components/TeamSection';
-import PricingSection from '../components/PricingSection';
-import BlogSection from '../components/BlogSection';
-import Card3DCarousel from '../components/Card3DCarousel';
-import ClientsCarousel from '../components/ClientsCarousel';
+import React from "react";
+import HeroSection from "../components/HeroSection";
+import SplitHeroSection from "../components/SplitHeroSection";
+import EventCarousel from "../components/EventCarousel";
+import FeaturedEventsCarousel from "../components/FeaturedEventsCarousel";
+import EventJourneyCarousel from "../components/EventJourneyCarousel";
+import FeatureCards from "../components/FeatureCards";
+import StatsSection from "../components/StatsSection";
+import ProcessSection from "../components/ProcessSection";
+import TestimonialsSection from "../components/TestimonialsSection";
+import FAQSection from "../components/FAQSection";
+import CTASection from "../components/CTASection";
+import VideoBackgroundSection from "../components/VideoBackgroundSection";
+import GallerySection from "../components/GallerySection";
+import TeamSection from "../components/TeamSection";
+import PricingSection from "../components/PricingSection";
+import BlogSection from "../components/BlogSection";
+import Card3DCarousel from "../components/Card3DCarousel";
+import ClientsCarousel from "../components/ClientsCarousel";
 
 const Home: React.FC = () => {
   return (
     <div>
       <HeroSection />
-      <SplitHeroSection />
-    
       <EventCarousel />
       <FeaturedEventsCarousel />
       <EventJourneyCarousel />
@@ -32,7 +30,7 @@ const Home: React.FC = () => {
       <GallerySection />
       <TeamSection />
       <ProcessSection />
-        <Card3DCarousel />
+      <Card3DCarousel />
       <ClientsCarousel />
       <PricingSection />
       <TestimonialsSection />
